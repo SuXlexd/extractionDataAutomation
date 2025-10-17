39a3b2e6de097f50dee0ec046d56bd954e54b0f3
SuXlexd <alejandroocampo66@aragon.unam.mx>
2025-10-17 10:36:51 -0600

    Delete scrap_mt/mercadoLibre directory

--- scrap_mt/mercadoLibre/src/utils/procedures/get_env_variables.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def get_env_variables(prefix):
-    return {
-        'database': os.getenv(f'DATABASE_{prefix}'),
-        'password': os.getenv(f'PASSWORD_{prefix}'),
-        'user': os.getenv(f'USER_{prefix}'),
-        'host': os.getenv(f'HOST_{prefix}'),
-    }
